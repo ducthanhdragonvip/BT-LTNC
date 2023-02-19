93953277e1a39f9b3c6cc09980ce4278256302f0
ducthanhdragonvip <122378571+ducthanhdragonvip@users.noreply.github.com>
2023-02-20 00:12:29 +0700

    Update B.cpp

--- BT2/B.cpp
+++ BT2/B.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <iomanip>
 using namespace std;
 int main()
 {
@@ -8,10 +9,10 @@ int main()
     string n="So tien nhan sau thue: ";
     float baoHiem=luongThang*9/100;
     float tienChiuThue=luongThang-baoHiem;
-    if(tienChiuThue<=1000000) cout<<n<<tienChiuThue;
-    else if(tienChiuThue>1000000 && tienChiuThue<=1500000) cout<<n<<fixed<<1000000+(tienChiuThue-1000000)*90/100;
-    else if(tienChiuThue>1500000 && tienChiuThue<=2000000) cout<<n<<fixed<<1000000+450000+(tienChiuThue-1500000)*85/100;
-    else if(tienChiuThue>2000000) cout<<n<<fixed<<1000000+450000+425000+(tienChiuThue-2000000)*80/100;
+    if(tienChiuThue<=1000000) cout<<n<<fixed<<setprecision(0)<<tienChiuThue;
+    else if(tienChiuThue>1000000 && tienChiuThue<=1500000) cout<<n<<fixed<<setprecision(0)<<1000000+(tienChiuThue-1000000)*90/100;
+    else if(tienChiuThue>1500000 && tienChiuThue<=2000000) cout<<n<<fixed<<setprecision(0)<<1000000+450000+(tienChiuThue-1500000)*85/100;
+    else if(tienChiuThue>2000000) cout<<n<<fixed<<setprecision(0)<<1000000+450000+425000+(tienChiuThue-2000000)*80/100;
     cout<<endl;
     
     return 0;
